f951041f172be41a1d0a7714ca41b3a3376aead2
cdceballor <35697740+cdceballor@users.noreply.github.com>
2024-08-06 17:10:13 -0500

    Update useGetAllUsers.ts

--- src/hooks/useGetAllUsers.ts
+++ src/hooks/useGetAllUsers.ts
@@ -1,35 +1,60 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ENDPOINT } from "../constants";
 import { GetUsers } from "../interfaces";
 
-const useGetAllUsers=()=> {
+const useGetAllUsers = () => {
   const [users, setUsers] = useState<GetUsers[]>([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<null | string>(null);
-    const fetchingUsers = async () => {
-      setLoading(true);
-      try{
-          fetch(`${ENDPOINT}/users`)
-            .then(response => response.json())
-            .then(json => setUsers(json))
-      }
-      catch(error){
-          setError(error instanceof Error ? error.message : String(error))
-      }
-      finally{
-        setLoading(false);
-      }
+  const [usersToFilter, setUsersToFilter] = useState<GetUsers[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<null | string>(null);
+
+  const fetchingUsers = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${ENDPOINT}/users`);
+      const json = await response.json();
+      setUsers(json);
+      setUsersToFilter(json);
+    } catch (error) {
+      setError(error instanceof Error ? error.message : String(error));
+    } finally {
+      setLoading(false);
     }
-
-    const HandleDeleteUser = (id: number) => {
-    setUsers(users.filter((user) => user.id !== id));
   };
-  
-      useEffect(()=>{
-        fetchingUsers()
-    },[])
 
-      return {users, loading, error, HandleDeleteUser}
-}
+  const handleMemoizedDeleteUser = useCallback(
+    (id: number) => {
+      setUsers(users.filter((user) => user.id !== id));
+    },
+    [users]
+  );
 
-export default useGetAllUsers
+  const handleMemoizedFilterUserByName = useCallback(
+    (name: string) => {
+      if (name === "") {
+        setUsersToFilter(users);
+      } else {
+        setUsersToFilter((prevUsers) =>
+          prevUsers.filter((user) =>
+            user.name.toLowerCase().includes(name.toLowerCase())
+          )
+        );
+      }
+    },
+    [users]
+  );
+
+  useEffect(() => {
+    fetchingUsers();
+  }, []);
+
+  return {
+    users: usersToFilter,
+    error,
+    loading,
+    handleMemoizedDeleteUser,
+    handleMemoizedFilterUserByName,
+  };
+};
+
+export default useGetAllUsers;
