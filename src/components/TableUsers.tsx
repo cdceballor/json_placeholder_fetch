67db548fe12b8c3eb99686b47f53b7fac2e22238
cdceballor <35697740+cdceballor@users.noreply.github.com>
2024-08-06 17:09:51 -0500

    Update TableUsers.tsx

--- src/components/TableUsers.tsx
+++ src/components/TableUsers.tsx
@@ -4,35 +4,55 @@ import { GetUsers } from "../interfaces";
 interface TableUsersProps {
   users: GetUsers[];
   handleDeleteUser: (id: number) => void;
+  handleSearchChange: (value: string) => void;
 }
-const TableUsers = ({ users, handleDeleteUser }: TableUsersProps) => {
+
+const TableUsers = ({
+  users,
+  handleDeleteUser,
+  handleSearchChange,
+}: TableUsersProps) => {
   return (
-    <table>
-      <tr>
-        <th>Name</th>
-        <th>Username</th>
-        <th>Email</th>
-        <th>Phone</th>
-        <th>Website</th>
-        <th>Action</th>
-      </tr>
-      {users
-        .sort((a, b) => a.id - b.id)
-        .map((user, key) => {
-          return (
-            <tr key={key}>
-              <td>{user.name}</td>
-              <td>{user.username}</td>
-              <td>{user.email}</td>
-              <td>{user.phone}</td>
-              <td>{user.website}</td>
-              <td>
-                <button onClick={() => handleDeleteUser(user.id)}>🗑️</button>
-              </td>
-            </tr>
-          );
-        })}
-    </table>
+    <>
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        onChange={(e) => handleSearchChange(e.target.value)}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Website</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users
+            .sort((a, b) => a.id - b.id)
+            .map((user, key) => {
+              return (
+                <tr key={key}>
+                  <td>{user.name}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                  <td>{user.phone}</td>
+                  <td>{user.website}</td>
+                  <td>
+                    <button onClick={() => handleDeleteUser(user.id)}>
+                      🗑️
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </>
   );
 };
 
